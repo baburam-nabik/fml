49299d0fe519bc8951d7726a4ab95c4542f4b5c8
baburam-nabik <baburam_nabik@bpointer.in>
2025-05-12 15:03:56 +0530

    first commit 3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1252329854"
+    serviceWorkerVersion: "3261286785"
   }
 });
